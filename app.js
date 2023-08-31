1739c78740eb8fc4be337404a1464c02530f60c0
konain1 <kaunainkasab@gmail.com>
2023-09-01 02:29:36 +0530

    97.5 hours left

--- app.js
+++ app.js
@@ -1,4 +1,26 @@
 
-const final = document.querySelector('.finalValue')
+const finalValue = document.querySelector('.finalValue')
 
-console.log(final   )
+const maxValue = document.querySelector('.maxValue')
+
+const stick = document.querySelector('.stick')
+
+let max = 0;
+let final = 0;
+
+function changeWidth(){
+    stick.style.width = `${final / max * 100}%`
+    stick.innerHTML = `${final/ max * 100}%`
+
+}
+finalValue.addEventListener('keyup',()=>{
+    final = Number(finalValue.value)
+    changeWidth()
+})
+
+maxValue.addEventListener('keyup',()=>{
+    max =  Number(maxValue.value)
+    changeWidth()    
+
+})
+
